7b6c14f65d28cce3b81374c7ca477bd928294792
PrzemyslawBorczak1 <przemyslawborczak@gmail.com>
2026-05-22 14:23:21 +0200

    Main Page

--- src/frontend/src/pages/MainPage.tsx
+++ src/frontend/src/pages/MainPage.tsx
@@ -1,1 +1,21 @@
-export { default } from './main';
+import Navbar from '../components/layout/Navbar';
+import Footer from '../components/layout/Footer';
+import Hero from './main/sections/Hero';
+import Benefits from './main/sections/Benefits';
+import AddRoute from './main/sections/AddRoute';
+import LoginCTA from './main/sections/LoginCTA';
+
+export default function MainPage() {
+  const loggedIn = !!localStorage.getItem('token');
+
+  return (
+    <main className="min-h-screen bg-[#eaf6df]">
+      <Navbar />
+      <Hero loggedIn={loggedIn} />
+      <Benefits />
+      <AddRoute />
+      <LoginCTA />
+      <Footer />
+    </main>
+  );
+}
