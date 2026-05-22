e8dd71f884ef357c165818dbdafe1e0094aff822
PrzemyslawBorczak1 <przemyslawborczak@gmail.com>
2026-05-22 13:26:19 +0200

    Cleaned app tsx

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,12 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginRegisterPage from './pages/LoginRegisterPage';
 import MainPage from './pages/MainPage';
-import TripForm from './previous-app/pages/TripForm';
-import Trips from './previous-app/pages/Trips';
-import TripDetails from './previous-app/pages/TripDetails';
-import Chat from './previous-app/pages/Chat';
-import Messages from './previous-app/pages/Messages';
-import MyTrips from './previous-app/pages/MyTrips';
 
 export default function App() {
   return (
@@ -16,13 +10,6 @@ export default function App() {
         <Route path="/login" element={<LoginRegisterPage />} />
         <Route path="/register" element={<LoginRegisterPage />} />
         <Route path="/home" element={<MainPage />} />
-        <Route path="/trip/create" element={<TripForm />} />
-        <Route path="/trips" element={<Trips />} />
-        <Route path="/trips/:id" element={<TripDetails />} />
-        <Route path="/chat/:receiverId" element={<Chat />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/messages/:partnerId" element={<Messages />} />
-        <Route path="/my-trips" element={<MyTrips />} />
       </Routes>
     </BrowserRouter>
   );
