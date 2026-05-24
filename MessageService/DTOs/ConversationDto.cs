1374541744058256e93109a7dd07304d49e95e2a
scorelowless <makochonmarek@gmail.com>
2026-05-24 11:02:54 +0200

    added DTO for last message

--- MessageService/DTOs/ConversationDto.cs
+++ MessageService/DTOs/ConversationDto.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 
 namespace MessageService.DTOs;
 
+public class LastMessageDto
+{
+    public Guid MessageId { get; set; }
+    public Guid SenderId { get; set; }
+    public string? Type { get; set; }
+    public System.Text.Json.Nodes.JsonObject? Payload { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
+
 public class ConversationDto
 {
     public Guid ConversationId { get; set; }
@@ -10,5 +19,5 @@ public class ConversationDto
     public string? Name { get; set; }
     public DateTime? Date { get; set; }
     public List<Guid> Participants { get; set; } = new();
+    public LastMessageDto? LastMessage { get; set; }
 }
-
