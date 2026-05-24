837f85c6956974c27e58ac199bd14a1591a588b8
scorelowless <makochonmarek@gmail.com>
2026-05-24 11:03:42 +0200

    Added validator for Message DTO

---
+++ MessageService/Validation/CreateMessageDtoValidator.cs
@@ -0,0 +1,15 @@
+using FluentValidation;
+using MessageService.DTOs;
+
+namespace MessageService.Validation;
+
+public class CreateMessageDtoValidator : AbstractValidator<CreateMessageDto>
+{
+    public CreateMessageDtoValidator()
+    {
+        RuleFor(x => x.Type).NotEmpty().WithMessage("type is required");
+        RuleFor(x => x.Payload).NotNull().WithMessage("payload is required");
+        RuleFor(x => x.Payload).Must(p => p != null).WithMessage("payload must be an object");
+    }
+}
+
