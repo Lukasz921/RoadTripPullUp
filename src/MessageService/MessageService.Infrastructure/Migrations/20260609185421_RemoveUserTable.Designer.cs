86c784599a452e08ad2d0b9e33a5985eb701a531
scorelowless <makochonmarek@gmail.com>
2026-06-09 20:54:54 +0200

    Added migration related to removal of the concept of user from MessageService

--- src/MessageService/MessageService.Infrastructure/Migrations/20260609185421_RemoveUserTable.Designer.cs
+++ src/MessageService/MessageService.Infrastructure/Migrations/20260609185421_RemoveUserTable.Designer.cs
@@ -4,6 +4,7 @@ using System.Text.Json.Nodes;
 using MessageService.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MessageService.Infrastructure.Migrations
 {
     [DbContext(typeof(MessagesDbContext))]
-    partial class MessagesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260609185421_RemoveUserTable")]
+    partial class RemoveUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
