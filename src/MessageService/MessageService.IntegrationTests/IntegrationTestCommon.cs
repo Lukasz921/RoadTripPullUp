168c896f53ed77e4070ff4e8ad59e204896499ff
scorelowless <makochonmarek@gmail.com>
2026-06-13 22:48:44 +0200

    Implement IntegrationTestCommon methods

--- src/MessageService/MessageService.IntegrationTests/IntegrationTestCommon.cs
+++ src/MessageService/MessageService.IntegrationTests/IntegrationTestCommon.cs
@@ -1,24 +1,55 @@
+using System.Net.Http.Json;
+using System.Text.Json.Nodes;
+
 namespace MessageService.IntegrationTests;
 
-public class IntegrationTestCommon
+public static class IntegrationTestCommon
 {
-    public async Task<Guid> CreateConversation2Members()
+    public static async Task<Guid> CreateConversation2Members(HttpClient client)
     {
-        return await Task.FromResult(Guid.NewGuid());
+        return await CreateConversationNMembers(2, client);
     }
     
-    public async Task<Guid> CreateConversation0Members()
+    public static async Task<Guid> CreateConversation0Members(HttpClient client)
     {
-        return await Task.FromResult(Guid.NewGuid());
+        return await CreateConversationNMembers(0, client);
     }
     
-    public async Task<Guid> CreateMessage(Guid conversationId)
+    public static async Task<Guid> CreateMessage(Guid conversationId, HttpClient client)
     {
-        return await Task.FromResult(Guid.NewGuid());
+        var createMessageDto = new
+        {
+            ConversationId = conversationId,
+            Payload = new JsonObject { ["text"] = "Hello, world!" },
+            Type = "text"
+        };
+        
+        var messageResponse = await client.PostAsJsonAsync("/api/v1/message/messages", createMessageDto);
+        var response2 = await messageResponse.Content.ReadFromJsonAsync<Dictionary<string, Guid>>();
+        var messageId = response2?["messageId"] != null ? Guid.Parse(response2["messageId"].ToString()) : Guid.Empty;
+        return messageId == Guid.Empty ? throw new Exception("Failed to create message for test") : messageId;
     }
 
-    private async Task<Guid> CreateConversationNMembers(int n)
+    private static async Task<Guid> CreateConversationNMembers(int n, HttpClient client)
     {
-        return await Task.FromResult(Guid.NewGuid());
+        List<Guid> members = [];
+        for (int i = 0; i < n; i++)
+        {
+            members.Add(Guid.NewGuid());
+        }
+        
+        var createConversationDto = new
+        {
+            TripId = Guid.NewGuid(),
+            Title = "Test Conversation",
+            Date = DateTime.UtcNow,
+            Participants = members
+        };
+        var response = await client.PostAsJsonAsync("/api/v1/message/conversations", createConversationDto);
+        var parsedResponse = await response.Content.ReadFromJsonAsync<Dictionary<string, object>>();
+        var conversationId = parsedResponse?["conversationId"] != null
+            ? Guid.Parse(parsedResponse["conversationId"].ToString() ?? "")
+            : Guid.Empty;
+        return conversationId == Guid.Empty ? throw new Exception("Failed to create conversation for test") : conversationId;
     }
 }
