488cd7d88e817891c2fdf260bba5791e8d9a65df
Łukasz Przybylski <01189155@pw.edu.pl>
2026-06-14 17:04:02 +0200

    unban_removed

--- src/Users/Application/Interfaces/IUserService.cs
+++ src/Users/Application/Interfaces/IUserService.cs
@@ -9,7 +9,6 @@ public interface IUserService
     Task Update(Guid id, UpdateUserDTO dto);
     Task UpdateUserRating(Guid userId, int rating);
     Task Ban(Guid userId, BanUserDTO dto);
-    Task Unban(Guid userId);
     Task ChangeRole(Guid userId, UserRole newRole);
     Task<UserIntegrationDTO> GetUserIntegrationData(Guid id);
     Task FileComplaint(Guid complainerId, Guid tripId, FileComplaintDTO dto);
